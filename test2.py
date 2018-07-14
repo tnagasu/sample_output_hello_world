4f5b56d0e9199dcaf97338ee56b3b97000890c82
Nagasu Tomoyuki <tnagasu@d2.dion.ne.jp>
2018-07-14 22:27:09 +0900

    "Hello world"を各種書式で標準出力(10種類作成)

--- test2.py
+++ test2.py
@@ -1,3 +1,40 @@
-if __name__ == '__main__':
+def print_def():
     print("Hello world!")
-    print("Good morning.")
+
+
+if __name__ == '__main__':
+    # print()関数
+    print("Hello world!")
+
+    # print(f書式)関数
+    print(f"Hello world!")
+
+    # 変数(定数)を使用
+    MESSAGE_CONST = "Hello world!"
+    print(MESSAGE_CONST)
+
+    # 変数を使用
+    message_variable = "Hello world!"
+    print(message_variable)
+
+    # 変数とprint(f書式)を使用
+    print(f"{message_variable}")
+
+    # リストとprint()関数を使用
+    list_ = ("Hello world!")
+    print(list_)
+
+    # リストとprint(f書式)を使用
+    list_ = ("Hello world!")
+    print(f"{list_}")
+
+    # タプルとprint()関数を使用
+    tuple_ = ["Hello world!"]
+    print(tuple_[0])
+
+    # タプルとprint(f書式)を使用
+    tuple_ = ["Hello world!"]
+    print(f"{tuple_[0]}")
+
+    # 関数をコール
+    print_def()
