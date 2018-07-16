7cd5a6f39c9455419e7ee512c53e4a83053a1956
Nagasu Tomoyuki <tnagasu@d2.dion.ne.jp>
2018-07-16 20:22:02 +0900

    Add 6 samples. Add comment messages.

--- sample_output_hello_world.py
+++ sample_output_hello_world.py
@@ -1,14 +1,56 @@
+#!/usr/bin/env python3
+'''output "Hello world!" to standard output.
+Input : None
+return : None
+'''
+
+
+# -----------------------------------------------------------------------------
+#  Functions
+# -----------------------------------------------------------------------------
+
 def print_def():
     print("Hello world!")
 
+
+# -----------------------------------------------------------------------------
+#  Main script
+# -----------------------------------------------------------------------------
 
 if __name__ == '__main__':
     # print()関数
     print("Hello world!")
 
     # print(end='')
+    print("Hello ", end='')
+    print("world!")
+
+    # print(end=' ')
     print("Hello", end=' ')
     print("world!")
+
+    # print(.format notation)
+    message = "Hello world!"
+    print("{0}" .format(message))
+
+    # print(.format notation 2 variables)
+    message1 = "Hello"
+    message2 = "world!"
+    print("{0} {1}" .format(message1, message2))
+
+    # print(% notation)
+    message = "Hello world!"
+    print("%s" % message)
+
+    # print(% notation 2 variables
+    message1 = "Hello"
+    message2 = "world!"
+    print("%s %s" % (message1, message2))
+
+    # print(+ notation)
+    message1 = "Hello"
+    message2 = "world!"
+    print(message1 + " " + message2)
 
     # print(f書式)関数
     print(f"Hello world!")
